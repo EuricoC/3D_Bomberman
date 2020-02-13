9d0fd1805d95467a7763b667a62aa51642978215
Eurico Carajote <C17442362@mydit.ie>
2020-02-13 09:21:50 +0000

    Update

---
+++ Unity tutorial_1stUnityPrj/rollerball/Rollerball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+    public float mouseSpeed;
+    private int count;
+    
+    public Text countText;
+    public Text endText;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        setCountText();
+        endText.text =  "";
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(moveHorizontal,0.0f,moveVertical);
+       
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            setCountText();
+            
+        }
+    }
+
+    private void OnMouseDrag()
+    {
+        float x = Input.GetAxis("Mouse X")*mouseSpeed * Mathf.Deg2Rad;
+        transform.Rotate(Vector3.up, x);
+        
+    }
+
+    void setCountText()
+    {
+        countText.text = "Score: " + count.ToString();
+        if (count >= 8)
+        {
+            endText.text = "You Win";
+        }
+    }
+}
